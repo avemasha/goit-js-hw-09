abc0039fcea96bb297f8db3cb38d8c11735ffa09
Maria Novosad <100295231+avemasha@users.noreply.github.com>
2022-12-24 10:17:38 +0100

    finally

---
+++ src/js/03-promises.js
@@ -0,0 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+
+let currentDelay = Number(delay.value)
+const currentStep = Number(step.value)
+const currentAmount = Number(amount.value)
+
+for (let i =1; i <= currentAmount; i +=1){
+  createPromise(i, currentDelay)
+  .then(({position, delay}) => {
+    Notify.success(`Fulfilled promise ${position} in ${delay} ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`Rejected promise ${position} in ${delay} ms`);
+  });
+  currentDelay += currentStep;
+}
+});
+
+
+function createPromise(position, delay) {
+  return new Promise ((resolve, reject) => {
+  const shouldResolve = Math.random() > 0.3;
+setTimeout(() => {
+  if (shouldResolve) {
+    resolve({ position, delay });
+  } 
+    reject({position, delay})
+  }, delay);
+})
+};
+
